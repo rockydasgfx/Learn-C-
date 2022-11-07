fa754e658b39a4a74265a2eb4977dbe0d4a4e447
Rocky Das <76746655+rockydasgfx@users.noreply.github.com>
2022-11-08 00:51:26 +0600

    Data type, variable, operator

---
+++ 1.datatype, variable, operator.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //first Program 
+  cout << "Hello World" << endl;
+  cout << "Again Hello World \n";
+/*
+  //Data Type & Variable 
+  int age = 21;
+  cout << age << endl;
+
+  char nameFirstWord = 'R';
+  cout << nameFirstWord << endl;
+
+  bool isTrue = false;
+  cout << isTrue << endl;
+
+  float numTwo = 20.55;
+  cout << numTwo << endl;
+
+  double numThree = 21.84;
+  cout << numThree << endl;
+*/
+  
+/*
+  //variable mamory get size 
+  int ageSize = sizeof(age); 
+  cout << ageSize << endl;
+
+  int nameFirstWordSize = 
+  sizeof(nameFirstWord); 
+  cout << nameFirstWordSize << endl;
+
+  int isTrueSize = sizeof(isTrue); 
+  cout << isTrueSize << endl;
+
+  int numTwoSize = sizeof(numTwo); 
+  cout << numTwoSize << endl;
+
+  int numThreeSize = sizeof(numThree); 
+  cout << numThreeSize << endl;
+*/
+
+/*
+  // type  casting 
+  int a = 'a';
+  cout << a << endl;
+
+  char ch = 97 ;
+  cout << ch << endl;
+
+  char ch2 = 123456;
+  cout << ch2 << endl;
+*/
+
+/*
+  // Operator 
+  int add = 30 + 20;
+  cout << add << endl;
+  
+  int sub = 30 - 20;
+  cout << sub << endl;
+
+  int multi = 5 * 4;
+  cout << multi << endl;
+
+  // int/int = int, float/int = float, duoble/int = duoble and declared expected data type. 
+  int div = 4/5;
+  cout << div << endl;
+
+  float divTwo = 4.22/5;
+  cout << divTwo << endl;
+
+  int modulus = 5/4;
+  cout << modulus << endl;
+
+  int inc = ++add;
+  cout << inc << endl;
+
+  int dec = --sub;
+  cout << dec << endl;
+  
+  int a = 2;
+  int b = 3;
+
+  bool grea = a > b;
+  cout << grea << endl;
+
+  bool less = a < b;
+  cout << less << endl;
+
+  //>= & <=
+
+  bool equalTo  = a == b;
+  cout << equalTo << endl; 
+  
+ bool notEqual = a != b;
+ cout << notEqual << endl; 
+
+  //logical &&, ||,! 
+*/
+
+}
